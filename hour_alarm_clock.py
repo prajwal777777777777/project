9a2f229b8826bf7b31706e2b4c7dbfe443431149
prajwal777777777777 <66990884+prajwal777777777777@users.noreply.github.com>
2020-12-02 21:08:43 +0545

    Update hour_alarm_clock.py

--- hour_alarm_clock.py
+++ hour_alarm_clock.py
@@ -1,21 +1,30 @@
 import pyinputplus as pypi;
 try:
     choice=pypi.inputYesNo(prompt="Do you want time in 12 hour format[Y/N]:").lower()
+    
     if(choice=="yes" or choice=="y"):
         current_time=pypi.inputInt(prompt="Enter the current time in hours:",max=12);
+    
     else:
         current_time=pypi.inputInt(prompt="Enter the current time in hours:",max=24);
+    
+    
     while True:
         am_pm=pypi.inputMenu(["AM","PM"]).lower();
-        if((choice=="no" or choice=="n") and (current_time>=12) and (am_pm=="am")):
+        
+        if((choice=="no" or choice=="n") and (current_time>=12 and current_time<24) and (am_pm=="am")):
             print(f"{current_time} cannot be am in 24 hour format.");
             pass;
             #In 24 hour format time cannot be greater than 12 and am.(i.e 13 am is not possible) and continue the infinite loop.
+        
+        elif((choice=="no" or choice=="n") and (current_time==24) and (am_pm=="pm")):
+            print(f"{current_time} cannot be pm in 24 hour format.")
         else:
             break;
             #if eveything is good it will break infinite loop.
 
     alarm=pypi.inputInt(prompt="Enter the time for alarm in hours:",max=100000);
+    
     backup=am_pm
     #we assign a backup variable equals to am_pm variable which will need in future.
 
@@ -24,6 +33,7 @@ try:
     if(days>0):
         days_new=days
         pass;
+    
     else:
         days_new=0
     hours=alarm%24 #max value=23
@@ -38,11 +48,25 @@ try:
         
         conditioned_list=list(filter(lambda x:x%12==0,alarm_list));
         #we filterd list if the element of list gives remainder 0 when divided by 12(i.e it would only contain number divisible by 12.)
+        if(current_time<12 and alarm<12 and len(conditioned_list)==0 and (alarm_time>=12 and alarm_time<=22)):
+            global am_pm
+            
+            if(am_pm=="am"):
+               am_pm="pm"
+            
+            else:
+               am_pm="am"
+        
+        elif(current_time==23 and alarm<12 and len(conditioned_list)==0 and alarm_time<=34):
+            
+            if(am_pm=="pm"):
+                am_pm="am"
+        else:
+            pass;
         
         for i in range(1,len(conditioned_list)+1):
             #it would iterate over the list from 1.
 
-            global am_pm;
             global backup;
             #we use global variable am_pm and backup for our local use.
 
@@ -50,19 +74,26 @@ try:
                 #we are doing if alarm time less than 12 it would be am and if it is greater than 12 and less than 24 it would be pm.
                 #e.g supose current_time is 7am and we set alarm after 23 hours then our condtion_list contain on 12[12] cuz till 23, 12 is only divisible by 12.
                 #so our below odd even if else statment cannot change am to pm so if the alarm time is 23 hours so it is greater than 12 then it would change am_pm. 
+                
                 if(am_pm=="am"):
                     am_pm="pm";
+                
                 else:
                     am_pm=="am";
                 #it would change am to pm(in nutshell it would change am to pm if condtion list contain single element.) .
+           
             if(i%2!=0):
                 #if len of condition_list element is greater than 1 for every odd element it would change am to pm and vice-versa.
+                
                 if(am_pm=="am"):
                     am_pm="pm";
+                
                 else:
                     am_pm="am";
+            
             else:
                 am_pm=backup;#if element of list is even it would be same like if current_time is 7am and i set alarm after 24 hours first it would change am to pm and again pm to am .
+        
         am_pm_function.ext=am_pm
         #to use in local variable outside function
     am_pm_function()
@@ -75,34 +106,50 @@ try:
     if(choice=="yes" or choice=="y"):
         def twelve_hour():
             if(alarm_time>=24): #alarm_time max=23+12=35
+                
                 remaing_time=alarm_time-24
+                
                 if(remaing_time>12):
                     remaing_time=remaing_time-12
-                    print("Alarm will go off in "+str(days_new)+"days"+f" {str(hours)}hours"+" in "+str(remaing_time)+str(am_pm_function.ext));
+                    
+                    if(am_pm=="am"):
+                        print("Alarm will go off in "+str(days_new)+"days"+f" {str(hours)}hours"+" in "+str(remaing_time)+str(am_pm_function.ext));
+                
                 elif(remaing_time==0):
+                    
                     remaing_time=12
+                    
                     print("Alarm will go off in "+str(days_new)+"days"+f" {str(hours)}hours"+" in "+str(remaing_time)+str(am_pm_function.ext))
+                
                 else:
                     print("Alarm will go off in "+str(days_new)+"days"+f" {str(hours)}hours"+" in "+str(remaing_time)+str(am_pm_function.ext));
             elif(alarm_time>12 and alarm_time<24):
+                
                 remaing_time=alarm_time-12
+                
                 print("Alarm will go off in "+str(days_new)+"days"+f" {str(hours)}hours"+" in "+str(remaing_time)+str(am_pm_function.ext));
+            
             else:
                 print("Alarm will go off in "+str(days_new)+"days"+f" {str(hours)} hours"+" in"+str(alarm_time)+str(am_pm_function.ext));
 
         twelve_hour();
     else:
         def twenty_four():
+            
             if(alarm_time>=24):
                 twenty_four_remaing_time=alarm_time-24
+                
                 if(twenty_four_remaing_time>=12):
                     am_pm_function.ext="pm"
+                
                 if(twenty_four_remaing_time==0):
                     twenty_four_remaing_time=24
                     am_pm_function.ext="am"
                     print("Alarm will go off after "+str(days_new)+"days"+f"{str(hours)}hours"+" in "+str(twenty_four_remaing_time)+str(am_pm_function.ext));                
+                
                 else:
                     print("Alarm will go off in "+str(days_new)+"days"+f" {str(hours)}hours"+" in "+str(twenty_four_remaing_time)+str(am_pm_function.ext))
+            
             else:
                 print("Alarm will go off in "+str(days_new)+"days"+f" {str(hours)}hours"+" in "+str(alarm_time)+str(am_pm_function.ext));
         twenty_four()
@@ -112,3 +159,4 @@ except:
     print("An unknown error has occured.");
     quit;
                                     #try yourself
+  
